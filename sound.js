f173573d7869df01616f8a17413f62ca87feb8ba
emmakosy <143273271+emmakosy@users.noreply.github.com>
2024-04-09 15:56:48 +0100

    initial sound_box files

---
+++ sound.js
@@ -0,0 +1,46 @@
+
+    document.querySelector(".arcade-retro-game").addEventListener("click", handleClick);
+
+    function handleClick(){
+        var arcaderetrogame = new Audio('mixkit-arcade-retro-game-over-213.wav');
+        arcaderetrogame.play();
+    } 
+    document.querySelector(".fast-rocket-whoosh").addEventListener("click", handleClick1);
+
+    function handleClick1(){
+        var fastrocketwhoosh = new Audio('mixkit-fast-rocket-whoosh-1714.wav');
+        fastrocketwhoosh.play();
+    } 
+
+    document.querySelector(".classic-alarm").addEventListener("click", handleClick2);
+
+    function handleClick2(){
+        var classicalarm = new Audio('mixkit-classic-alarm-995.wav');
+        classicalarm.play();
+    }
+    document.querySelector(".retro-game-emergency-alarm").addEventListener("click", handleClick3);
+
+    function handleClick3(){
+        var retrogameemergencyalarm = new Audio('mixkit-retro-game-emergency-alarm-1000.wav');
+        retrogameemergencyalarm.play();
+    }
+    
+    document.querySelector(".retro-game-notification").addEventListener("click", handleClick4);
+
+    function handleClick4(){
+        var retrogamenotification = new Audio('mixkit-retro-game-notification-212.wav');
+        retrogamenotification.play();
+    }
+    document.querySelector(".aggressive-beast-roar").addEventListener("click", handleClick5);
+
+    function handleClick5(){
+        var aggressivebeastroar = new Audio('mixkit-aggressive-beast-roar-13.wav');
+        aggressivebeastroar.play();
+    }
+     document.querySelector(".trumpet-fanfare").addEventListener("click", handleClick6); 
+            
+    function handleClick6(){
+        var trumpetfanfare = new Audio('mixkit-trumpet-fanfare-2293.wav');
+        trumpetfanfare.play();
+    }
+    
